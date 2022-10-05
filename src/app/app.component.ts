b96716a8f4e67ee91dceef05ebfc2387c9db854e
nigelmarongere <nigelmarongere@gmail.com>
2022-10-05 12:08:20 +0100

    reactive form working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reactive-form';
+  reactiveForm: FormGroup;
+
+  attendees = new Array();
+
+  ngOnInit(){
+    this.reactiveForm = new FormGroup({
+      fullname: new FormControl(null),
+      company: new FormControl(null),
+      email: new FormControl(null),
+      region: new FormControl(null),
+      role: new FormControl(null)
+    });
+  }
+
+  onSubmit(){
+    this.attendees.push(this.reactiveForm.value);
+    console.log(this.attendees);
+  }
 }
